9761a2f7b855fcd757135523414ab9162344a264
semhartesfamariam <49767489+semhartesfamariam@users.noreply.github.com>
2019-06-01 19:43:47 -0700

    Update Artifact05.js

--- assignments/Artifact05/Artifact05.js
+++ assignments/Artifact05/Artifact05.js
@@ -1,2 +1,26 @@
 
+body {
+background-Color:lightpink;
+}
+h2 {
+    text-align: center;
+}
+
+h1 {
+    text-align: center;
+    color:black;
+}
+
+img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 80%;
+}
 
