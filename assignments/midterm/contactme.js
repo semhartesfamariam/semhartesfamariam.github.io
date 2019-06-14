3119318ea6a5e9be91dda70a1a0012a76f179ff0
semhartesfamariam <49767489+semhartesfamariam@users.noreply.github.com>
2019-06-14 12:46:13 -0700

    Update contactme.js

--- assignments/midterm/contactme.js
+++ assignments/midterm/contactme.js
@@ -1,59 +1,14 @@
-var frmvalidator = new Validator("myform");
-frmvalidator.addValidation("Name","req","Please enter your Name");
-frmvalidator.addValidation("Name","maxlen=70",
-       "Max length for Name is 70");
-
- frmvalidator.addValidation("Email","req", "Please enter your email");
- frmvalidator.addValidation("Email","maxlen=50",
-      "Max lenght for Email is 50 characters");
-
-
  var frmvalidator = new Validator("myform");
- frmvalidator.addValidation("Comment","req","Please enter your comment here");
+    frmvalidator.addValidation("FirstName","req","Please enter your First Name");
+      frmvalidator.addValidation("FirstName","maxlen=20",
+                                 "Max length for FirstName is 20");
 
-//Validate the name
- function validateForm(){
-   var LETTERS = /^[A-Za-z]+$/;
-   var NUMBERS = /[0-9]/g;
-   var errorMessages= "";
-   //1) Create variable
-   var validname=false;
-   //2) read value from HTML
-   var name = document.getElementById("Name").value;
-   //3) Do validation
-   if (name==="null" || name==="" || name.length > 20  )
-       errorMessages += "<p>The name is required and can only accept alphabetical values and maximum 20 characters</p>";
-   else
-   {
-   //  if(firstname.match(^[a-zA-Z\\s]*$))
-   if (name.match(NUMBERS) == null)
-        {
-          validname = true;
-        }
-      else
-        {
+      frmvalidator.addValidation("LastName","req");
+      frmvalidator.addValidation("LastName","maxlen=20");
 
-          errorMessages += "<p>The name can only accept alphabetical values</p>";
-           validname = false;
-        }
-    }
+      frmvalidator.addValidation("Email","maxlen=50");
+      frmvalidator.addValidation("Email","req");
+      frmvalidator.addValidation("Email","email");
+    
+     frmvalidator.addValidation("Comment","maxlen=50");
 
-//***to validate the email
-var vaildeEmail=false;
-var userEmail = document.getElementById("Email").value;
-var atpos = userEmail.indexOf("@");
-var dotpos = userEmail.lastIndexOf(".");
-if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=userEmail.length)
-  errorMessages +="<p>Invalid email</p>";
-else
- validEmail = true;
-    // send error message. For example:  errorMessages = "<p>Invalid email</p>";
-//document.getElementById("errorMessages").innerHTML = errorMessages.toString();
-    //return validEmail; //Or assign the value to a variable. For example validEmail = true;
-    //return (validFirstname && validLastname && validEmail);
-
-
-document.getElementById("errorMessages").innerHTML = errorMessages.toString();
-
-  return (validname && validEmail);
-}
